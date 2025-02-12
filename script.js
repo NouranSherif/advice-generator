aae9ac76a4d70c979fdbb330aaa0d018329a4672
Nouran Sherif <nouransherif901@gmail.com>
2025-02-12 10:36:17 +0200

    add loader

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const btn = document.querySelector('.btn');
 const pEl = document.getElementById('advice');
 const adviceNum = document.getElementById('adviceNum');
+const loader = document.getElementById('loader');
 
 btn.addEventListener('click', () => {
   getAdvice();
@@ -9,6 +10,10 @@ const getAdvice = async () => {
   const res = await fetch('https://api.adviceslip.com/advice');
   data = await res.json();
   await displayAdvice(data.slip);
+
+  pEl.innerHTML === ''
+    ? loader.classList.remove('d-none')
+    : loader.classList.add('d-none');
 };
 
 const displayAdvice = advice => {
